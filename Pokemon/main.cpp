8b4fdfd563b64c627e017938cb7f1fd72ff1adb4
Kainat Ali <129224803+Kainat475@users.noreply.github.com>
2025-06-07 21:05:32 +0500

    My first Pokemon!

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -8,11 +8,12 @@ int main() {
 
     cout << "\nEnter your name: ";
     cin >> player_name;
-    cout << "\n" << endl;
+    cout << "\n";
 
     // Meeting Professor Oak
-    cout << "Someone in a lab coat: Hi, " << player_name << "! Welcome to the world of POKEMON!My name is Professor Oak. \n";
-    cout << "I will be helping you start with your first pokemon. I give you three choices to choose from:";
+    cout << "Professor Oak: Ah, a Trainer. Hi, " << player_name << "! \nWelcome to the world of POKEMON!My name is Professor Oak. \n";
+    cout << "Today is a momentous day -- I will be helping you choose your first Pokemon.\nEvery great Trainer remembers this day for the rest of their lives.\n";
+    cout << "I give you three choices to choose from. So, choose wisely, young one. Your options are: ";
     cout << "\n\t1. Bulbasaur\n\t2. Charmander\n\t3. Squirtle\n";
     cout << "Which one would you like? \n\n";
 
@@ -36,9 +37,11 @@ int main() {
     {
         cout << "\nProfessor Oak: Based on your input, I believe it's safe to assume that you do NOT want any of the choices I've provided.\n";
         cout << "Professor Oak: Goodbye and Good day.\n\n\n";
+        return 0;
     }
 
-
+    cout << "And an excellent one. But beware, Trainer, for this is only the beginning.\nYour journey is about to unfold. Now let's see if you've got what it takes to keep going.\n";
+    cout << "Good luck, and remember... Choose wisely!\n";
 
     return 0;
 }
