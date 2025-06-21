5f4a2f4687dc3279841cf5e9ef9d6016c3d4166a
Kainat Ali <kainatali080@gmail.com>
2025-06-22 01:26:31 +0500

    Created Player, Pokemon, and Professor Oak Classes.
    I removed and deleted previously used if and switch statements since they were for practice only. Added OOP concepts instead to implement classes and call their instances in the main function instead.

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 // -------------------------------------------------------------------------------------------- //
@@ -10,142 +11,105 @@ enum class PokemonChoice {
     Bulbasaur,
     Charmander,
     Squirtle,
-    InvalidChoice
+    Pikachu //Dafault
 };
 
-// Selecting Pokemon using enums
-PokemonChoice enum_Class_Pokemon_Choosing(PokemonChoice pokemonAssignedHere, int choice)
-{
-    // Implementation using Switch statements
-    switch (choice)
-    {
-        case 1:
-        {
-            pokemonAssignedHere = PokemonChoice::Bulbasaur;
-            cout << "\nProfessor Oak: A fine choice! Bulbasaur is always ready to grow on you! \n";
-            break;
-        }
-        case 2:
-        {
-            pokemonAssignedHere = PokemonChoice::Charmander;
-            cout << "\nProfessor Oak: A fiery choice! Charmander is yours! \n";
-            break;
-        }
-        case 3:
-        {
-            pokemonAssignedHere = PokemonChoice::Squirtle;
-            cout << "\nProfessor Oak: Splendid! Squirtle will keep you cool under pressure! \n";
-            break;
-        }
-        default:
-        {
-            pokemonAssignedHere = PokemonChoice::Charmander;
-            cout << "\nProfessor Oak: Hmm, that doesn't seem right. Let me choose for you… Charmander seems to be perfect for you. A fiery beast!\n";
-            break;
-        }
-    }
-    return pokemonAssignedHere;
-}
-
-
-//// ENUM structure
-enum PokemonChoiceEnum {    
-    Bulbasaur,
-    Charmander,
-    Squirtle,
-    InvalidChoice
+enum class PokemonType {
+    FIRE,
+    ELECTRIC,
+    WATER,
+    EARTH
 };
 
+class Pokemon {
+public:
+    string name;
+    int health;
+    PokemonType type;
 
-// Selecting Pokemon using enums
-PokemonChoiceEnum enum_Pokemon_Choosing(PokemonChoiceEnum pokemonAssignedHere, int choice)
-{
-    // Implementation using Switch statements
-    switch (choice)
-    {
-    case 1:
-    {
-        pokemonAssignedHere = Bulbasaur;
-        cout << "\nProfessor Oak: A fine choice! Bulbasaur is always ready to grow on you! \n";
-        break;
-    }
-    case 2:
-    {
-        pokemonAssignedHere = Charmander;
-        cout << "\nProfessor Oak: A fiery choice! Charmander is yours! \n";
-        break;
-    }
-    case 3:
-    {
-        pokemonAssignedHere = Squirtle;
-        cout << "\nProfessor Oak: Splendid! Squirtle will keep you cool under pressure! \n";
-        break;
-    }
-    default:
-    {
-        pokemonAssignedHere = Charmander;
-        cout << "\nProfessor Oak: Hmm, that doesn't seem right. Let me choose for you… Charmander seems to be perfect for you. A fiery beast!\n";
-        break;
-    }
-    }
-    return pokemonAssignedHere;
-}
+    // Default Constructur
+    Pokemon() {
 
-// if-else allow to test values against specific conditions and blocks
-int if_Else_Pokemon_Choosing(int choice)
-{
-    // Implementation using If-else statements
-    if (choice == 1)
-    {
-        cout << "\nProfessor Oak: A fine choice! Bulbasaur is always ready to grow on you! \n";
     }
-    else if (choice == 2)
-    {
-        cout << "\nProfessor Oak: A fiery choice! Charmander is yours!\n";
-    }
-    else if (choice == 3)
-    {
-        cout << "\nProfessor Oak: Splendid! Squirtle will keep you cool under pressure! \n";
-    }
-    else
-    {
-        cout << "\nProfessor Oak: Based on your input, I believe it's safe to assume that you do NOT want any of the choices I've provided.\n";
-        cout << "Professor Oak: Goodbye and Good day.\n\n\n";
-        return 1;
-    }
-    return 0;
-}
 
-// Compared to if-else, switch statements are clear and make code more readable
-// It is a cleaner and readable way to replace if-else conditions and helps de-cluttering the code.
-int switch_Pokemon_Choosing(int choice)
-{
-    // Implementation using Switch statements
-    switch (choice)
-    {
-        case 1:
-        {
-            cout << "\nProfessor Oak: A fine choice! Bulbasaur is always ready to grow on you! \n";
-            break;
-        }
-        case 2:
-        {
-            cout << "\nProfessor Oak: A fiery choice! Charmander is yours! \n";
-            break;
-        }
-        case 3:
-        {
-            cout << "\nProfessor Oak: Splendid! Squirtle will keep you cool under pressure! \n";
-            break;
-        }
-        default:
-        {
-            cout << "\nProfessor Oak: Hmm, that doesn't seem right. Let me choose for you… Just kidding! Let's go with Pikachu, the surprise guest!\n";
-            break;
-        }
+    // Parametrised Constructor
+    Pokemon(string n, int h, PokemonType t) {
+        name = n;
+        health = h;
+        type = t;
     }
-    return 0;
-}
+
+    // What functions would a Pokemon Perform?
+    void attack() {
+        cout << "My Pokemon did a Powerful attack move!\n";
+    }
+};
+
+class Player {
+public:
+    string playerName;
+    Pokemon chosenPokemon;
+
+    Player() {
+
+    }
+
+    Player(string name, Pokemon pokemon) {
+        this->playerName = name;
+        this->chosenPokemon = pokemon;  
+    }
+
+    // To Sleect a pokemon
+    void choosePokemon(int choice)
+    {
+        switch ((PokemonChoice)choice)
+        {
+            // converted choice to PokemonCHoiceEnum enum class object to find pokemon
+            case PokemonChoice::Bulbasaur:
+                chosenPokemon = Pokemon("Bulbasaur", 100, PokemonType::EARTH);
+                break;
+            case PokemonChoice::Charmander:
+                chosenPokemon = Pokemon("Charmander", 100, PokemonType::FIRE);
+                break;
+            case PokemonChoice::Squirtle:
+                chosenPokemon = Pokemon("Squirtle", 100, PokemonType::WATER);
+                break;
+            default:
+                cout << "\nAhh... A hidden choice. Surprise!";
+                chosenPokemon = Pokemon("Pikachu", 100, PokemonType::ELECTRIC);
+                break;
+        }
+        cout << "\nPlayer " << playerName << " chose " << chosenPokemon.name << "!\n";
+    }
+};
+
+class ProfessorOak {
+    public:
+    string name;
+
+    // Method to greet the player
+    void greetPlayer() {
+        cout << name << ": Ah, a Trainer. Hello there! Welcome to the world of Pokemon!\n";
+        cout << name << ": My name is Oak. People call me the Pokemon Professor!\n";
+        cout << name << ": But enough about me. Let's talk about you!\n";
+    }
+
+    // Method to offer the player Pokemon Choices
+    void offerPokemonChoices(Player& player) {
+        cout << name << ": Let's start with your name.\n";
+        getline(cin, player.playerName);
+        cout << name << ": " << player.playerName << ", what a fantastic name!\n\n";
+
+        cout << name << ": Today is a momentous day -- I will be helping you choose your first Pokemon. Every great Trainer remembers this day for the rest of their lives.\n";
+        cout << name << ": Now, I give you three choices to choose from. So, choose wisely, young one. Your options are: ";
+        cout << "\n\t1. Bulbasaur\n\t2. Charmander\n\t3. Squirtle\n";
+        cout << name << ": Which one would you like? Let me know your choice number and we'll begin.\n\n";
+
+        int choice;
+        cin >> choice;
+        player.choosePokemon(choice);
+    }
+};
 
 // -------------------------------------------------------------------------------------------- //
 // ------------------------------------  MAIN STARTS HERE  ------------------------------------ //
@@ -153,47 +117,16 @@ int switch_Pokemon_Choosing(int choice)
 
 int main() {
     
-    // Player Game Start Code
-    string player_name;    
-
-    cout << "\nEnter your name: ";
-    cin >> player_name;
-    cout << "\n";
-
-    // Meeting Professor Oak
-    cout << "Professor Oak: Ah, a Trainer. Hi, " << player_name << "! Welcome to the world of POKEMON!My name is Professor Oak. \n";
-    cout << "Today is a momentous day -- I will be helping you choose your first Pokemon. Every great Trainer remembers this day for the rest of their lives.\n";
-    cout << "I give you three choices to choose from. So, choose wisely, young one. Your options are: ";
-    cout << "\n\t1. Bulbasaur\n\t2. Charmander\n\t3. Squirtle\n";
-    cout << "Which one would you like? \n\n";
-
-    int choice = 0;
-    cout << player_name << ": ";
-    cin >> choice;
-
-    int function_returned = 0;
-
-    // Function if we want to select pokemon using if-else
-    //function_returned = if_Else_Pokemon_Choosing(choice);
+    ProfessorOak professorOak;
+    professorOak.name = "Professor Oak";
+    Pokemon playerPokemon = Pokemon("Pikachu", 100, PokemonType::ELECTRIC);
+    Player player = Player("Trainer", playerPokemon);
     
-    // Function for if we want choose Pokemon using switch statements 
-    //function_returned = switch_Pokemon_Choosing(choice);
+    // Callinf funstions for convo with Professor Oak
+    professorOak.greetPlayer();
+    professorOak.offerPokemonChoices(player);
 
-    // Function for choosing Pokemon using an Enum
-    // Setting it to Invalid Choice initially for surprise pokemon assignment
-    //PokemonChoiceEnum player_choice = InvalidChoice;
-    //player_choice = enum_Pokemon_Choosing(player_choice, choice);
-
-    // Function for choosing Pokemon using an Enum Class
-    // Setting it to Invalid Choice initially for surprise pokemon assignment
-    PokemonChoice player_choice_enumclass = PokemonChoice::InvalidChoice;
-    player_choice_enumclass = enum_Class_Pokemon_Choosing(player_choice_enumclass, choice);
-
-    // Stopping code execution if invalid input
-    if (function_returned != 0)
-        return 0;
-    
-
+    // Conclusion
     cout << "\nProfessor Oak: Beware, Trainer, for this is only the beginning. Your journey is about to unfold. Now let's see if you've got what it takes to keep going.\n";
     cout << "Good luck, and Choose wisely. Remember, Trainer, you and your Pokemon are about to form an unstoppable bond!\n";
     cout << "Now, take your first step into the vast world of Pokemon…\n" << "Who knows what mysteries await you? ;D\n\n";
